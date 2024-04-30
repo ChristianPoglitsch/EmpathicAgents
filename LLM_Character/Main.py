1627f005b620b54e130bbe6106dc312ab32e5502
Christian Poglitsch <christian.poglitsch@tugraz.at>
2024-04-30 12:34:11 +0200

    - out comment llama init

--- LLM_Character/Main.py
+++ LLM_Character/Main.py
@@ -215,10 +215,10 @@ print("CUDA found " + str(torch.cuda.is_available()))
 messages = MessagesAI()
 
 # Load Llama 3
-import transformers
-model_id = "meta-llama/Meta-Llama-3-8B"
-pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.getenv("HF_TOKEN"))
-pipeline("Hey how are you doing today?")
+#import transformers
+#model_id = "meta-llama/Meta-Llama-3-8B"
+#pipeline = transformers.pipeline("text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto", token=os.getenv("HF_TOKEN"))
+#pipeline("Hey how are you doing today?")
 
 
 
