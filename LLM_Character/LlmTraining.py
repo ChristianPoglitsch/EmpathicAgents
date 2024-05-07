39c0e504b12ace0675ea848870ae25c962e780f5
Christian Poglitsch <christian.poglitsch@tugraz.at>
2024-05-07 13:49:56 +0200

    - code to train mistral model

--- LLM_Character/LlmTraining.py
+++ LLM_Character/LlmTraining.py
@@ -201,6 +201,118 @@ def RunTrainedModel(model_id, trained_path):
     print(tokenizer.decode(outputs[0], skip_special_tokens=True))
 
 
+# --- Mistral training functions (https://exnrt.com/blog/ai/mistral-7b-fine-tuning/), (https://huggingface.co/mistralai/Mistral-7B-v0.1/discussions/133)
+def LoadMistralExampleDataset():
+    instruct_tune_dataset = load_dataset("mosaicml/instruct-v3")
+    print(instruct_tune_dataset)
+    created_prompt = create_prompt(instruct_tune_dataset["train"][0])
+    print(created_prompt)
+    
+    nf4_config = BitsAndBytesConfig(
+       load_in_4bit=True,
+       bnb_4bit_quant_type="nf4",
+       bnb_4bit_use_double_quant=True,
+       bnb_4bit_compute_dtype=torch.bfloat16
+    )
+
+    model = AutoModelForCausalLM.from_pretrained(
+        "mistralai/Mistral-7B-Instruct-v0.1",
+        device_map='auto',
+        quantization_config=nf4_config,
+        use_cache=False  
+    )
+
+    tokenizer = AutoTokenizer.from_pretrained("mistralai/Mistral-7B-v0.1")
+
+    tokenizer.pad_token = tokenizer.eos_token
+    tokenizer.padding_side = "right"
+    
+    generated_text = generate_response("### Instruction:\nUse the provided input to create an instruction that could have been used to generate the response with an LLM.\n\n### Input:\nI think it depends a little on the individual, but there are a number of steps you’ll need to take.  First, you’ll need to get a college education.  This might include a four-year undergraduate degree and a four-year doctorate program.  You’ll also need to complete a residency program.  Once you have your education, you’ll need to be licensed.  And finally, you’ll need to establish a practice.\n\n### Response:", model, tokenizer)
+    print(generated_text)
+    
+    # --- train model ---
+    peft_config = LoraConfig(
+        lora_alpha=16,
+        lora_dropout=0.1,
+        r=64,
+        bias="none",
+        task_type="CAUSAL_LM",
+        target_modules=["q_proj", "k_proj", "v_proj", "o_proj","gate_proj"]
+    )
+    model = prepare_model_for_kbit_training(model)
+    model = get_peft_model(model, peft_config)
+    
+    from transformers import TrainingArguments
+    args = TrainingArguments(
+      output_dir = "mistral_instruct_generation",
+      #num_train_epochs=5,
+      max_steps = 100, # comment out this line if you want to train in epochs
+      per_device_train_batch_size = 4,
+      warmup_steps = 0.03,
+      logging_steps=10,
+      save_strategy="epoch",
+      #evaluation_strategy="epoch",
+      evaluation_strategy="steps",
+      eval_steps=20, # comment out this line if you want to evaluate at the end of each epoch
+      learning_rate=2e-4,
+      bf16=True,
+      lr_scheduler_type='constant',
+    )
+    
+    max_seq_length = 2048
+    
+    from trl import SFTTrainer
+    trainer = SFTTrainer(
+      model=model,
+      peft_config=peft_config,
+      max_seq_length=max_seq_length,
+      tokenizer=tokenizer,
+      packing=True,
+      formatting_func=create_prompt,
+      args=args,
+      train_dataset=instruct_tune_dataset["train"],
+      eval_dataset=instruct_tune_dataset["test"]
+    )
+
+    trainer.train()
+    trainer.save_model("exnrt_mistral_instruct")
+
+
+
+def create_prompt(sample):
+    bos_token = "<s>"
+    original_system_message = "Below is an instruction that describes a task. Write a response that appropriately completes the request."
+    system_message = "Use the provided input to create an instruction that could have been used to generate the response with an LLM."
+    response = sample["prompt"].replace(original_system_message, "").replace("\n\n### Instruction\n", "").replace("\n### Response\n", "").strip()
+    input = sample["response"]
+    eos_token = "</s>"
+
+    full_prompt = ""
+    full_prompt += bos_token
+    full_prompt += "### Instruction:"
+    full_prompt += "\n" + system_message
+    full_prompt += "\n\n### Input:"
+    full_prompt += "\n" + input
+    full_prompt += "\n\n### Response:"
+    full_prompt += "\n" + response
+    full_prompt += eos_token
+
+    return full_prompt
+
+
+def generate_response(prompt, model, tokenizer):
+    encoded_input = tokenizer(prompt,  return_tensors="pt", add_special_tokens=True)
+    model_inputs = encoded_input.to('cuda')
+
+    generated_ids = model.generate(**model_inputs, max_new_tokens=1000, do_sample=True, pad_token_id=tokenizer.eos_token_id)
+
+    decoded_output = tokenizer.batch_decode(generated_ids)
+
+    return decoded_output[0].replace(prompt, "")
+
+
+# --- main function ---
+
 #path = os.getcwd() 
 ## Extracting all the contents in the directory corresponding to path
 #l_files = os.listdir(path) 
@@ -212,4 +324,6 @@ trained_path = "C:\\Development/LLM_Character/trained/thisserand/outputs_health_
 
 #RundModel(model_id)
 #TrainModel(model_id, trained_path)
-RunTrainedModel(model_id, trained_path)
+#RunTrainedModel(model_id, trained_path)
+
+LoadMistralExampleDataset()
