e8a1bc63b7b845cff634f1a1828ad9ddf5e411a3
IbrahimElk <ibrahim.elkaddouri@student.kuleuven.be>
2024-08-08 15:24:01 +0200

    temp commit

--- LLM_Character/persona/prompt_templates/planning_prompts/determine_action/action_sector.py
+++ LLM_Character/persona/prompt_templates/planning_prompts/determine_action/action_sector.py
@@ -3,14 +3,16 @@ Given the action description, persona,
 return the suitable sector where this action can take place.   
 """
 import sys
-sys.path.append('../../../')
+sys.path.append('../../../../')
 
 from LLM_Character.llm_api import LLM_API 
-from persona.persona import Persona
+from LLM_Character.persona.persona import Persona
+import LLM_Character.persona.prompt_templates.prompt as p 
 
-def _create_prompt_input(self, action_description:str, persona:Persona): 
+COUNTER_LIMIT = 5
+
+def _create_prompt_input(action_description:str, persona:Persona): 
    
-    # GATHER ALL THE NECASSARY INFORMATION. 
     act_world = persona.scratch.get_curr_location()['world']
     act_sector = persona.scratch.get_curr_location()['sector']
     domicile = persona.scratch.get_living_area()['sector']
@@ -20,9 +22,8 @@ def _create_prompt_input(self, action_description:str, persona:Persona):
     possible_arenas = persona.s_mem.get_str_accessible_sector_arenas(act_world, domicile)
    
     daily_plan = persona.scratch.get_str_daily_plan_req() 
-    action_description_1, action_description_2 = self._decomp_action_desc(action_description)
+    action_description_1, action_description_2 = _decomp_action_desc(action_description)
     
-    # PUT ALL THE NECESSARY INFORMATION INTO A LIST, IN THE RIGHT ORDER ACCORDING TO THE PROMPT TEMPLATE. 
     prompt_input = []
     prompt_input += [name]
     prompt_input += [domicile]
@@ -30,10 +31,7 @@ def _create_prompt_input(self, action_description:str, persona:Persona):
     prompt_input += [name]
     prompt_input += [act_sector]
     prompt_input += [possible_arenas]
-    #TODO: check if this is correct, normaliter, zou er geen \n als dailyplan "" leeg is, ma kdenk kan geen kwaad. 
     prompt_input += [f"\n{daily_plan}"]
-    #TODO: we moeten ons geen zorgen maken of de available sector in iemand anders zijn huis is, want we werken maar in 1 scene.
-    # geen zorgen hierover, in ons geval.  
     prompt_input += [possible_sectors]
 
     prompt_input += [name]
@@ -44,7 +42,7 @@ def _create_prompt_input(self, action_description:str, persona:Persona):
 
     return prompt_input
 
-def _decomp_action_desc(self, action_description:str):
+def _decomp_action_desc(action_description:str):
     if "(" in action_description: 
         action_description_1 = action_description.split("(")[0].strip()
         action_description_2 = action_description.split("(")[-1][:-1]
@@ -52,88 +50,54 @@ def _decomp_action_desc(self, action_description:str):
     else : 
         return action_description, action_description
 
-def _clean_up_response(self, response:str):
-    pass
- 
-def _validate_response(self, response:str):
-    pass
+def _clean_up_response(response:str):
+    cleaned_response = response.split("}")[0]
+    return cleaned_response
 
-def run_prompt(self, persona:Persona, model:LLM_API, verbose=False):
-    # model.safe_generate_response
-    pass
+def _validate_response(response:str): 
+    if len(response.strip()) < 1: 
+      return False
+    if "}" not in response:
+      return False
+    if "," in response: 
+      return False
+    return True
 
+def _get_fail_safe(): 
+    fs = ("kitchen")
+    return fs
 
+def _get_valid_output(model, prompt, counter_limit):
+    for _ in range(counter_limit):
+        output = model.query_text(prompt)
+        if _validate_response(output):
+            return _clean_up_response(output)
+    return _get_fail_safe()
+
+def run_prompt_action_sector(persona:Persona, model:LLM_API, action_description:str, verbose=False):
+    prompt_template = "LLM_Character/persona/prompt_template/action_sector.txt"
+    prompt_input = _create_prompt_input(action_description, persona)
+    prompt = p.generate_prompt(prompt_input, prompt_template)
+    output = _get_valid_output(model, prompt, COUNTER_LIMIT)
+
+    y = persona.scratch.curr_location['world']
+    x = [i.strip() for i in persona.s_mem.get_str_accessible_sectors(y).split(",")]
+    # NOTE why was this commented in the original repo ?? it makes sense to check...
+    # FIXME YOU DONT DO ANYTHING WITH THE OUTPUT ??? YOU MIGHT AS WELL RETURN persona.scratch.get_living_area ? 
+    # if output not in x: 
+# output = random.choice(x)
+    output = persona.scratch.get_living_area()['sector']
+
+    # if debug or verbose: 
+    # print_run_prompts(prompt_template, persona, gpt_param, 
+    #                   prompt_input, prompt, output)
+    return output, [output, prompt, prompt_input]
 
 
 if __name__ == "__main__":
-    x = ActionSector()
-    pass
+    person = Persona("FRERO")
+    model = None
+    run_prompt_action_sector(person, model, "i will drive to the broeltorens.")
 
 
-
-
-
-
-
-
-
-
-
-
-
-def run_gpt_prompt_action_sector(action_description, 
-                                persona, 
-                                maze, 
-                                test_input=None, 
-                                verbose=False):
-  def create_prompt_input(action_description, persona, maze, test_input=None): 
-    act_world = f"{maze.access_tile(persona.scratch.curr_tile)['world']}"
-    
-    prompt_input = []
-    
-    prompt_input += [persona.scratch.get_str_name()]
-    prompt_input += [persona.scratch.living_area.split(":")[1]]
-    x = f"{act_world}:{persona.scratch.living_area.split(':')[1]}"
-    prompt_input += [persona.s_mem.get_str_accessible_sector_arenas(x)]
-
-
-    prompt_input += [persona.scratch.get_str_name()]
-    prompt_input += [f"{maze.access_tile(persona.scratch.curr_tile)['sector']}"]
-    x = f"{act_world}:{maze.access_tile(persona.scratch.curr_tile)['sector']}"
-    prompt_input += [persona.s_mem.get_str_accessible_sector_arenas(x)]
-
-    if persona.scratch.get_str_daily_plan_req() != "": 
-      prompt_input += [f"\n{persona.scratch.get_str_daily_plan_req()}"]
-    else: 
-      prompt_input += [""]
-
-
-    # MAR 11 TEMP
-    accessible_sector_str = persona.s_mem.get_str_accessible_sectors(act_world)
-    curr = accessible_sector_str.split(", ")
-    fin_accessible_sectors = []
-    for i in curr: 
-      if "'s house" in i: 
-        if persona.scratch.last_name in i: 
-          fin_accessible_sectors += [i]
-      else: 
-        fin_accessible_sectors += [i]
-    accessible_sector_str = ", ".join(fin_accessible_sectors)
-    # END MAR 11 TEMP
-
-    prompt_input += [accessible_sector_str]
-
-
-
-    action_description_1 = action_description
-    action_description_2 = action_description
-    if "(" in action_description: 
-      action_description_1 = action_description.split("(")[0].strip()
-      action_description_2 = action_description.split("(")[-1][:-1]
-    prompt_input += [persona.scratch.get_str_name()]
-    prompt_input += [action_description_1]
-
-    prompt_input += [action_description_2]
-    prompt_input += [persona.scratch.get_str_name()]
-    return prompt_input
 
