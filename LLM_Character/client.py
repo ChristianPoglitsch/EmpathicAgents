3f59530e628adf2dd4b8dad00fdfab06bc9ca4d6
ibrahim <ibrahim.elkaddouri@student.kuleuven.be>
2024-08-22 14:43:28 +0200

    [ADD] a python client for casual testing

--- LLM_Character/client.py
+++ LLM_Character/client.py
@@ -28,163 +28,163 @@ nice = s.ReadReceivedData()
 print("Received1:")
 print(nice)
 
-# json2 = {
-#     "type" : "PromptMessage",
-#     "data" : {
-#         "persona_name" : "Camila",
-#         "user_name" : "Louis",
-#         "message" : "Hi"
-#     }
-# }
-# s.SendData(json.dumps(json2))
+json2 = {
+    "type" : "PromptMessage",
+    "data" : {
+        "persona_name" : "Camila",
+        "user_name" : "Louis",
+        "message" : "Hi"
+    }
+}
+s.SendData(json.dumps(json2))
 
-# time.sleep(20)
-# nice = s.ReadReceivedData()
-# print("Received2:")
-# print(nice) 
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received2:")
+print(nice) 
 
-# updateMetaJson = {
-#     "curr_time": "July 25, 2024, 09:15:45",
-#     "sec_per_step": 30
-#     }
+updateMetaJson = {
+    "curr_time": "July 25, 2024, 09:15:45",
+    "sec_per_step": 30
+    }
 
-# updatePersonaJson  = {
-#     "name" : "Camila", # old name
-#     "scratch_data": {
-#             "first_name": "Alice",
-#             "last_name": "Smith",
-#             "age": 30,
-#             "living_area": {
-#                     "world" : "Graz",
-#                     "sector" : "DownTown",
-#                     "arena" : "Someone's Appartement",
-#             },
-#             "recency_w": 5,
-#             "relevance_w": 7,
-#             "importance_ele_n": 3
-#         },
-#         "spatial_data": {
-#             "world": "FantasyLand",
-#             "sectors": [{
-#                 "sector": "Northern Realm",
-#                 "arenas": [
-#                 {
-#                     "arena": "Dragon's Lair",
-#                     "gameobjects": [
-#                     {"gameobject": "Dragon"},
-#                     {"gameobject": "Treasure Chest"}]
-#                 }, 
-#                 {
-#                     "arena": "Ice Cavern",
-#                     "gameobjects": [
-#                     {"gameobject": "Ice Golem"},
-#                     {"gameobject": "Frozen Statue"}]
-#                 }]
-#             }]
-#         }
-#     }
+updatePersonaJson  = {
+    "name" : "Camila", # old name
+    "scratch_data": {
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "age": 30,
+            "living_area": {
+                    "world" : "Graz",
+                    "sector" : "DownTown",
+                    "arena" : "Someone's Appartement",
+            },
+            "recency_w": 5,
+            "relevance_w": 7,
+            "importance_ele_n": 3
+        },
+        "spatial_data": {
+            "world": "FantasyLand",
+            "sectors": [{
+                "sector": "Northern Realm",
+                "arenas": [
+                {
+                    "arena": "Dragon's Lair",
+                    "gameobjects": [
+                    {"gameobject": "Dragon"},
+                    {"gameobject": "Treasure Chest"}]
+                }, 
+                {
+                    "arena": "Ice Cavern",
+                    "gameobjects": [
+                    {"gameobject": "Ice Golem"},
+                    {"gameobject": "Frozen Statue"}]
+                }]
+            }]
+        }
+    }
 
-# updateUserJson = {
-#     "old_name" : "Louis",
-#     "name" : "Joe Smith",
-# }
+updateUserJson = {
+    "old_name" : "Louis",
+    "name" : "Joe Smith",
+}
 
-# json3 = {
-#     "type" : "UpdateMetaMessage",
-#     "data" : updateMetaJson 
-# }
+json3 = {
+    "type" : "UpdateMetaMessage",
+    "data" : updateMetaJson 
+}
 
-# json4 = {
-#     "type" : "UpdatePersonaMessage",
-#     "data" : updatePersonaJson
-# }
+json4 = {
+    "type" : "UpdatePersonaMessage",
+    "data" : updatePersonaJson
+}
 
-# json5 = {
-#     "type" : "UpdateUserMessage",
-#     "data" : updateUserJson
-# }
+json5 = {
+    "type" : "UpdateUserMessage",
+    "data" : updateUserJson
+}
 
-# s.SendData(json.dumps(json3))
+s.SendData(json.dumps(json3))
 
-# time.sleep(20)
-# nice = s.ReadReceivedData()
-# print("Received3:")
-# print(nice) 
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received3:")
+print(nice) 
 
-# s.SendData(json.dumps(json4))
+s.SendData(json.dumps(json4))
 
-# time.sleep(20)
-# nice = s.ReadReceivedData()
-# print("Received4:")
-# print(nice) 
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received4:")
+print(nice) 
 
-# s.SendData(json.dumps(json5))
+s.SendData(json.dumps(json5))
 
-# time.sleep(20)
-# nice = s.ReadReceivedData()
-# print("Received5:")
-# print(nice) 
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received5:")
+print(nice) 
 
-# AddPersonaJson  = {
-#     "name" : "Alice Smith", # new name 
-#     "scratch_data": {
-#             "curr_location" : {
-#                 "world" : "Graz",
-#                 "sector" : "JakominiPlatz",
-#                 "arena" : "Home"
-#             },
-#             "first_name": "Alice",
-#             "last_name": "Smith",
-#             "age": 30,
-#             "innate": "Curious",
-#             "learned": "Programming",
-#             "currently": "Researcher",
-#             "lifestyle": "Urban",
-#             "living_area": {
-#                     "world" : "Graz",
-#                     "sector" : "DownTown",
-#                     "arena" : "Someone's Appartement",
-#             },
-#             "recency_w": 5,
-#             "relevance_w": 7,
-#             "importance_w": 9,
-#             "recency_decay": 2,
-#             "importance_trigger_max": 10,
-#             "importance_trigger_curr": 6,
-#             "importance_ele_n": 3
-#         },
-#         "spatial_data": {
-#             "world": "FantasyLand",
-#             "sectors": [{
-#                 "sector": "Northern Realm",
-#                 "arenas": [
-#                 {
-#                     "arena": "Dragon's Lair",
-#                     "gameobjects": [
-#                     {"gameobject": "Dragon"},
-#                     {"gameobject": "Treasure Chest"}]
-#                 }, 
-#                 {
-#                     "arena": "Ice Cavern",
-#                     "gameobjects": [
-#                     {"gameobject": "Ice Golem"},
-#                     {"gameobject": "Frozen Statue"}]
-#                 }]
-#             }]
-#         }
-#     }
+AddPersonaJson  = {
+    "name" : "Alice Smith", # new name 
+    "scratch_data": {
+            "curr_location" : {
+                "world" : "Graz",
+                "sector" : "JakominiPlatz",
+                "arena" : "Home"
+            },
+            "first_name": "Alice",
+            "last_name": "Smith",
+            "age": 30,
+            "innate": "Curious",
+            "learned": "Programming",
+            "currently": "Researcher",
+            "lifestyle": "Urban",
+            "living_area": {
+                    "world" : "Graz",
+                    "sector" : "DownTown",
+                    "arena" : "Someone's Appartement",
+            },
+            "recency_w": 5,
+            "relevance_w": 7,
+            "importance_w": 9,
+            "recency_decay": 2,
+            "importance_trigger_max": 10,
+            "importance_trigger_curr": 6,
+            "importance_ele_n": 3
+        },
+        "spatial_data": {
+            "world": "FantasyLand",
+            "sectors": [{
+                "sector": "Northern Realm",
+                "arenas": [
+                {
+                    "arena": "Dragon's Lair",
+                    "gameobjects": [
+                    {"gameobject": "Dragon"},
+                    {"gameobject": "Treasure Chest"}]
+                }, 
+                {
+                    "arena": "Ice Cavern",
+                    "gameobjects": [
+                    {"gameobject": "Ice Golem"},
+                    {"gameobject": "Frozen Statue"}]
+                }]
+            }]
+        }
+    }
 
-# json6 = {
-#     "type" : "AddPersonaMessage",
-#     "data" : AddPersonaJson 
-# }
+json6 = {
+    "type" : "AddPersonaMessage",
+    "data" : AddPersonaJson 
+}
 
-# s.SendData(json.dumps(json6))
+s.SendData(json.dumps(json6))
 
-# time.sleep(20)
-# nice = s.ReadReceivedData()
-# print("Received6:")
-# print(nice) 
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received6:")
+print(nice) 
 
 
 MoveJson = {
@@ -193,29 +193,51 @@ MoveJson = {
     "arena" : "Bäckerei Sorger",
 }
 
-json7 = {
-    "type" : "MoveMessage",
-    "data" : {
-        "Camila": MoveJson
-    } 
+EventData1 = {
+    "action_event_subject" : "Florian",
+    "action_event_predicate" : None,
+    "action_event_object" : None,
+    "action_event_description" : None
 }
 
+EventData2 = {
+    "action_event_obj_subject" : "Hammer",
+    "action_event_obj_spredicate" : None,
+    "action_event_obj_sobject" : None,
+    "action_event_obj_sdescription" : None
+}
 
+json7 = {
+    "type" : "MoveMessage",
+    "data" : [{
+        "name" : "Camila",
+        "curr_loc" : MoveJson,
+        "events" : [EventData1]
+    }] 
+}
+    
 s.SendData(json.dumps(json7))
 
-time.sleep(20)
+time.sleep(40)
 nice = s.ReadReceivedData()
 print("Received7:")
 print(nice) 
 
 
+json8 = {
+    "type" : "PromptMessage",
+    "data" : {
+        "persona_name" : "Alice Smith",
+        "user_name" : "Joe Smith",
+        "message" : "Hi"
+    }
+}
+s.SendData(json.dumps(json8))
 
-
-
-
-
-
-
+time.sleep(20)
+nice = s.ReadReceivedData()
+print("Received8:")
+print(nice) 
 
 
 # FIXME: hier zat ik : + nog unity endpoint developpen, maar dat zal wss voor weekend zijn?  
