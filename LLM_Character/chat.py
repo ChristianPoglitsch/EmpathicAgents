ea2d02f73dfee2d99d9a44628a975f73d7f90ec5
Christian Poglitsch <chris.poglitsch@gmail.com>
2024-09-19 16:46:39 +0200

    - Include analysis of answer

--- LLM_Character/chat.py
+++ LLM_Character/chat.py
@@ -19,8 +19,10 @@ if __name__ == "__main__":
 
     logger.info("CUDA found " + str(torch.cuda.is_available()))
 
-    #message = AIMessage(message="Who are you?", role="user", class_type="MessageAI", sender="user")
-    message = AIMessage(message="I am going to tell you a short story about some people. At the end of this story a person will say or do something. When I have finished telling it I will ask you some questions about what happened in the story. The story is: John and Mary were sitting in the newspaper office, reading through a huge pile of hate mail. 'Obviously our readers liked your story', said John. Did John think the readers liked the story?", role="user", class_type="MessageAI", sender="user")
+    query_introduction = "I am going to tell you a short story about some people. At the end of this story a person will say or do something. When I have finished telling it I will ask you some questions about what happened in the story. The story is: John and Mary were sitting in the newspaper office, reading through a huge pile of hate mail. 'Obviously our readers liked your story', said John. Did John think the readers liked the story?"
+    
+    
+    message = AIMessage(message=query_introduction, role="user", class_type="MessageAI", sender="user")
     messages = AIMessages()
     messages.add_message(message)
 
@@ -28,12 +30,22 @@ if __name__ == "__main__":
     model_id = "mistralai/Mistral-7B-Instruct-v0.2"
     # model_id = "genericgod/GerMerge-em-leo-mistral-v0.2-SLERP"
 
-    model = OpenAIComms()
-    model_id = "gpt-4o"
+    # model = OpenAIComms()
+    # model_id = "gpt-4o"
 
     model.init(model_id)
     wrapped_model = LLM_API(model)
     
-    result = wrapped_model.query_text(messages)
+    model.max_tokens = 100
+    query_result = wrapped_model.query_text(messages)
+    print("Answer")
+    print(query_result)    
 
-    print(result)
+    query_instruction_analye = "Is the answer for this question correct. Question:'" + query_introduction + "' Answer:'" + query_result + "'"
+    message = AIMessage(message=query_instruction_analye, role="user", class_type="MessageAI", sender="user")
+    messages = AIMessages()
+    messages.add_message(message)    
+    query_result_analyse = wrapped_model.query_text(messages)
+
+    print("Analysed answer")
+    print(query_result_analyse)
